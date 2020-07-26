007208c6e6cf2eb21b3c516fe35241eeac9ab7f3
alex-bagnall <Niscowal@gmail.com>
2020-07-26 17:21:25 +0100

    Double values.

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,8 +1,16 @@
+import java.lang.*;
+import java.util.Scanner;
+
 public class HelloWorld{
 
 	public static void main(String[] args){
 
-		System.out.println("Hello world!");
+		Scanner inputVal = new Scanner(System.in);
+		System.out.println("Enter number");
 
+		Integer outputNumber = inputVal.nextInt();
+		Double outputDouble = Double.valueOf(outputNumber);
+		outputDouble = Math.pow(outputDouble, 2);
+		System.out.println(outputDouble);
 	}
 }
